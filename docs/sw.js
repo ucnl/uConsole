55f505819099bb96e46e0d4ca98d720ec4fbd112
AlekUnderwater <a.dikarev@unavlab.com>
2026-09-14 14:08:15 +0300

    Standalone PWA support

---
+++ docs/sw.js
@@ -0,0 +1,29 @@
+const CACHE = 'aws-v1';
+
+const ASSETS = [
+  './',
+  './index.html',
+  './styles.css',
+  './app.js',
+  './serial-core.js',
+  './i18n.js',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request);
+    })
+  );
+});
